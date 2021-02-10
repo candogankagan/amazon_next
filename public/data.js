e54a1225be004ca09c2b41c27b843ae28abe69ad
Kagan Candogan <candogankagan@gmail.com>
2021-02-10 03:29:50 +0300

    data object created

---
+++ public/data.js
@@ -0,0 +1,77 @@
+export const productData = {
+    row1: [
+        {
+            id: 1,
+            title: 'A Promised Land',
+            price: 29.99,
+            image:
+                'https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._AC_SX184_.jpg',
+            rating: 5,
+        },
+        {
+            id: 2,
+            title:
+                'A Promised LaKindle Paperwhite – Now Waterproof with 2x the Storage - 8 GB ',
+            price: 149.99,
+            image:
+                'https://m.media-amazon.com/images/I/51fExaOBCfL._AC_UY327_FMwebp_QL65_.jpg',
+            rating: 4,
+        },
+        {
+            id: 3,
+            title: 'If You Tell',
+            price: 19.99,
+            image: 'https://m.media-amazon.com/images/I/410K-S--pmL.jpg',
+            rating: 4,
+        },
+    ],
+
+    row2: [
+        {
+            id: 4,
+            title:
+                'Wyze Cam 1080p HD Indoor WiFi Smart Home Camera with Night Vision',
+            price: 24.99,
+            image:
+                'https://m.media-amazon.com/images/I/61B04f0ALWL._AC_UY327_FMwebp_QL65_.jpg',
+            rating: 2,
+        },
+        {
+            id: 5,
+            title: 'Zeskit 8K Ultra HD High Speed 48Gpbs HDMI Cable',
+            price: 19.99,
+            image:
+                'https://m.media-amazon.com/images/I/71n8Pdm-HkL._AC_UY327_FMwebp_QL65_.jpg',
+            rating: 4,
+        },
+        {
+            id: 6,
+            title: 'GoPro HERO7 Black',
+            price: 229.99,
+            image:
+                'https://m.media-amazon.com/images/I/61ZUZcb0YiL._AC_UY327_FMwebp_QL65_.jpg',
+            rating: 3,
+        },
+    ],
+
+    row3: [
+        {
+            id: 7,
+            title:
+                'Eyoyo 1D Wireless Ring Barcode Scanner, Compatible with Bluetooth Function',
+            price: 74.99,
+            image:
+                'https://m.media-amazon.com/images/I/61SRIE8sTgL._AC_UY327_FMwebp_QL65_.jpg',
+            rating: 5,
+        },
+        {
+            id: 'Twitter Inc',
+            title:
+                'Book Couch iPad Tablet Holder Novelty eReader Rest Sofa Pillow Stand Gift Idea',
+            price: 24.99,
+            image:
+                'https://m.media-amazon.com/images/I/61hKIWnhX7L._AC_UY327_FMwebp_QL65_.jpg',
+            rating: 3,
+        },
+    ],
+}
