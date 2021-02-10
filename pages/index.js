70cea6af17d2dfc6adc61c3148415b24f2f90128
Kagan Candogan <candogankagan@gmail.com>
2021-02-10 03:44:28 +0300

    comment deleted

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 import Home from '../components/Home'
 import Header from '../components/Header'
-// import { auth } from '../services/firebase'
-// import { useStateValue } from '../context/StateProvider'
 
 export default function home() {
     return (
